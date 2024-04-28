3e4811e5d0f11eef6a9cb6be308b24a472f089f2
rouakhadhar4 <147189044+rouakhadhar4@users.noreply.github.com>
2024-04-28 19:02:11 +0100

    Add files via upload

---
+++ Tp3java/src/module-info.java
@@ -0,0 +1,9 @@
+
+module Tp3java {
+	   requires javafx.controls;
+	    requires javafx.fxml;
+	    requires javafx.graphics;
+	    requires javafx.base;
+
+	    opens application to javafx.graphics, javafx.fxml, javafx.base;
+	}
